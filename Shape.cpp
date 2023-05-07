307d51a4e6695836826d6e84a959953172871611
MAYCON BALDOINO <mm3432@live.mdx.ac.uk>
2023-05-07 13:29:06 +0100

    Learnig

---
+++ Shape.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "      /|\\ " << endl;
+    cout << "     /_|_\\ " << endl;
+    cout << "    /__|__\\ " << endl;
+    cout << "   /___|___\\ " << endl;
+    cout << "  /____|____\\ " << endl;
+    cout << " /_____|_____\\ " << endl;
+    cout << "/______|______\\ " << endl;
+}
