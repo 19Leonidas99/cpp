3132478c7220b71f4229983c8c589fac768c55f4
MAYCON BALDOINO <mm3432@live.mdx.ac.uk>
2023-05-08 18:55:56 +0100

    playing with if statement

--- if_Statement.cpp
+++ if_Statement.cpp
@@ -2,14 +2,32 @@
 
 using namespace std;
 
+int getMax(int num1, int num2)
+{
+    int result;
+    
+
+    if (num1 < num2){
+        result = num2;
+    } else if(num1 == num2){
+          result = 0;
+    } else {
+        result = num1;
+    }
+
+    return result;
+}
+
 int main()
 {
     bool isSomething = true;
     bool isBrave = false;
 
     if(isSomething || isBrave){    /* && - and operators both need to be true*/
-        cout << "TRUE";            /* || - or operator one has tobe true */
+        cout << "TRUE" << endl;            /* || - or operator one has tobe true */
     } else {                       /* else if - more condition to check */
-        cout << "FALSE";           /* ! - negation operator ex.: !isSomething*/
+        cout << "FALSE" << endl;           /* ! - negation operator ex.: !isSomething*/
     }
+
+    cout << getMax(5, 5);
 }
