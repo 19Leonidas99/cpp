536b9b9f248297f3dc58ddad0a57ba9848fb8eb0
MAYCON BALDOINO <mm3432@live.mdx.ac.uk>
2023-05-08 19:08:31 +0100

    basic calculator on terminal made in c++

---
+++ simpleCalculator.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double Calculator()
+{
+    double num1, num2, result;
+    char op;
+    cout << "Enter num1: ";
+    cin >> num1; 
+
+    cout << "Enter operator: ";
+    cin >> op;
+
+    cout << "Enter num2: ";
+    cin >> num2;
+    
+    if (op == '+'){
+        result = num1 + num2;
+    } else if (op == '-'){
+        result = num1 - num2;
+    } else if (op == '*') {
+        result = num1 * num2;
+    }else if (op == '/'){
+        result = num1 / num2;
+    }
+
+    return result;
+}
+
+int main()
+{
+    cout << Calculator() << endl;
+}
